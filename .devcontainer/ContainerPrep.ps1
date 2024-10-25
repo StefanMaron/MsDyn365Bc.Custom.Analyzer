2ccca3e983b7dc81be8496353b6c97f6ee24110c
Stefan Maroń <stefanmaron@outlook.de>
2024-10-25 09:50:18 +0000

    cleanup

--- .devcontainer/ContainerPrep.ps1
+++ .devcontainer/ContainerPrep.ps1
@@ -1,30 +1,33 @@
 ./.vscode/LoadALLanguage.ps1;
 
-mkdir ../AlDebugProject
+$Path = Join-Path $PSScriptRoot '..' '..'
 
-touch ../AlDebugProject/app.json
-printf '{\n' >> ../AlDebugProject/app.json
-printf '  "id": "d700542d-5688-4e64-aecb-648fa385a652",\n' >> ../AlDebugProject/app.json
-printf '  "name": "ALProject1",\n' >> ../AlDebugProject/app.json
-printf '  "publisher": "Default Publisher",\n' >> ../AlDebugProject/app.json
-printf '  "version": "1.0.0.0"\n' >> ../AlDebugProject/app.json
-printf '}' >> ../AlDebugProject/app.json
+mkdir "$Path/AlDebugProject"
+mkdir "$Path/AlDebugProject/.vscode"
 
-touch ../AlDebugProject/test.al
-printf 'codeunit 1 MyCodeunit\n' >> ../AlDebugProject/test.al
-printf '{\n' >> ../AlDebugProject/test.al
-printf '    trigger OnRun()\n' >> ../AlDebugProject/test.al
-printf '    begin\n' >> ../AlDebugProject/test.al
-printf '\n' >> ../AlDebugProject/test.al
-printf '    end;\n' >> ../AlDebugProject/test.al
-printf '}' >> ../AlDebugProject/test.al
+touch "$Path/AlDebugProject/app.json"
+printf '{\n' >> "$Path/AlDebugProject/app.json"
+printf '  "id": "d700542d-5688-4e64-aecb-648fa385a652",\n' >> "$Path/AlDebugProject/app.json"
+printf '  "name": "ALProject1",\n' >> "$Path/AlDebugProject/app.json"
+printf '  "publisher": "Default Publisher",\n' >> "$Path/AlDebugProject/app.json"
+printf '  "version": "1.0.0.0"\n' >> "$Path/AlDebugProject/app.json"
+printf '}' >> "$Path/AlDebugProject/app.json"
 
-touch ../AlDebugProject/.vscode/settings.json
-printf '{\n' >> ../AlDebugProject/.vscode/settings.json
-printf '    "al.codeAnalyzers": [' >> ../AlDebugProject/.vscode/settings.json
-printf '        "/workspaces/BusinessCentral.LinterCop/BusinessCentral.LinterCop/bin/Debug/netstandard2.1/BusinessCentral.LinterCop.dll"' >> ../AlDebugProject/.vscode/settings.json
-printf '    ],' >> ../AlDebugProject/.vscode/settings.json
-printf '    "al.enableCodeAnalysis": true' >> ../AlDebugProject/.vscode/settings.json
-printf '}' >> ../AlDebugProject/.vscode/settings.json
+touch "$Path/AlDebugProject/test.al"
+printf 'codeunit 1 MyCodeunit\n' >> "$Path/AlDebugProject/test.al"
+printf '{\n' >> "$Path/AlDebugProject/test.al"
+printf '    trigger OnRun()\n' >> "$Path/AlDebugProject/test.al"
+printf '    begin\n' >> "$Path/AlDebugProject/test.al"
+printf '\n' >> "$Path/AlDebugProject/test.al"
+printf '    end;\n' >> "$Path/AlDebugProject/test.al"
+printf '}' >> "$Path/AlDebugProject/test.al"
 
-code ../AlDebugProject/
+touch "$Path/AlDebugProject/.vscode/settings.json"
+printf '{\n' >> "$Path/AlDebugProject/.vscode/settings.json"
+printf '    "al.codeAnalyzers": [' >> "$Path/AlDebugProject/.vscode/settings.json"
+printf "        `"$(Join-Path $PSScriptRoot '..')/MsDyn365Bc.Custom.Analyzer/bin/Debug/netstandard2.1/CustomCodeCop.dll`"" >> "$Path/AlDebugProject/.vscode/settings.json"
+printf '    ],' >> "$Path/AlDebugProject/.vscode/settings.json"
+printf '    "al.enableCodeAnalysis": true' >> "$Path/AlDebugProject/.vscode/settings.json"
+printf '}' >> "$Path/AlDebugProject/.vscode/settings.json"
+
+code "$Path/AlDebugProject/"
